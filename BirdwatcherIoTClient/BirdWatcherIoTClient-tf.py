74e7af3e8adad4e0131ac1ce171e3bd94a0a29cf
Marek Tyrpa <mtyrpa@outlook.com>
2019-05-28 21:41:31 -0500

    fixed labelmap

--- BirdwatcherIoTClient/BirdWatcherIoTClient-tf.py
+++ BirdwatcherIoTClient/BirdWatcherIoTClient-tf.py
@@ -13,7 +13,6 @@
 # https://github.com/EdjeElectronics/TensorFlow-Object-Detection-on-the-Raspberry-Pi/blob/master/Object_detection_picamera.py
 
 
-
 # Import packages
 import os
 import cv2
@@ -27,6 +26,8 @@ import sys
 # Set up camera constants
 IM_WIDTH = 1280
 IM_HEIGHT = 720
+#IM_WIDTH = 640    Use smaller resolution for
+#IM_HEIGHT = 480   slightly faster framerate
 
 # Select camera type (if user enters --usbcam when calling this script,
 # a USB webcam will be used)
@@ -38,8 +39,6 @@ args = parser.parse_args()
 if args.usbcam:
     camera_type = 'usb'
 
-#### Initialize TensorFlow model ####
-
 # This is needed since the working directory is the object_detection folder.
 sys.path.append('..')
 
@@ -48,6 +47,7 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 # Name of the directory containing the object detection module we're using
+#MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 MODEL_NAME = 'faster_rcnn_inception_v2_coco_2018_01_28'
 
 # Grab path to current working directory
@@ -55,7 +55,7 @@ CWD_PATH = os.getcwd()
 
 # Path to frozen detection graph .pb file, which contains the model that is used
 # for object detection.
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_CKPT = os.path.join(CWD_PATH,"model",'frozen_inference_graph.pb')
 
 # Path to label map file
 PATH_TO_LABELS = os.path.join(CWD_PATH,'labelmap','birds_labelmap.pbtxt')
@@ -101,136 +101,18 @@ detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 # Number of objects detected
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-#### Initialize other parameters ####
-
 # Initialize frame rate calculation
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-# Define inside box coordinates (top left and bottom right)
-TL_inside = (int(IM_WIDTH*0.1),int(IM_HEIGHT*0.35))
-BR_inside = (int(IM_WIDTH*0.45),int(IM_HEIGHT-5))
-
-# Define outside box coordinates (top left and bottom right)
-TL_outside = (int(IM_WIDTH*0.46),int(IM_HEIGHT*0.25))
-BR_outside = (int(IM_WIDTH*0.8),int(IM_HEIGHT*.85))
-
-# Initialize control variables used for pet detector
-detected_inside = False
-detected_outside = False
-
-inside_counter = 0
-outside_counter = 0
-
-pause = 0
-pause_counter = 0
-
-#### Bird Identifier function ####
-
-# This function contains the code to detect a bird, determine it's species.
-def bird_ID(frame):
-
-    # Use globals for the control variables so they retain their value after function exits
-    global detected_inside, detected_outside
-    global inside_counter, outside_counter
-    global pause, pause_counter
-
-    frame_expanded = np.expand_dims(frame, axis=0)
-
-    # Perform the actual detection by running the model with the image as input
-    (boxes, scores, classes, num) = sess.run(
-        [detection_boxes, detection_scores, detection_classes, num_detections],
-        feed_dict={image_tensor: frame_expanded})
-
-    # Draw the results of the detection (aka 'visulaize the results')
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        frame,
-        np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
-        np.squeeze(scores),
-        category_index,
-        use_normalized_coordinates=True,
-        line_thickness=8,
-        min_score_thresh=0.40)
-
-    
-    # Check the class of the top detected object by looking at classes[0][0].
-    # If the top detected object is a cat (17) or a dog (18) (or a teddy bear (88) for test purposes),
-    # find its center coordinates by looking at the boxes[0][0] variable.
-    # boxes[0][0] variable holds coordinates of detected objects as (ymin, xmin, ymax, xmax)
-    if (((int(classes[0][0]) == 17) or (int(classes[0][0] == 18) or (int(classes[0][0]) == 88))) and (pause == 0)):
-        x = int(((boxes[0][0][1]+boxes[0][0][3])/2)*IM_WIDTH)
-        y = int(((boxes[0][0][0]+boxes[0][0][2])/2)*IM_HEIGHT)
-
-        # Draw a circle at center of object
-        cv2.circle(frame,(x,y), 5, (75,13,180), -1)
-
-        # If object is in inside box, increment inside counter variable
-        if ((x > TL_inside[0]) and (x < BR_inside[0]) and (y > TL_inside[1]) and (y < BR_inside[1])):
-            inside_counter = inside_counter + 1
-
-        # If object is in outside box, increment outside counter variable
-        if ((x > TL_outside[0]) and (x < BR_outside[0]) and (y > TL_outside[1]) and (y < BR_outside[1])):
-            outside_counter = outside_counter + 1
-
-    # If pet has been detected inside for more than 10 frames, set detected_inside flag
-    # and send a text to the phone.
-    if inside_counter > 10:
-        detected_inside = True
-        message = client.messages.create(
-            body = 'Your pet wants outside!',
-            from_=twilio_number,
-            to=my_number
-            )
-        inside_counter = 0
-        outside_counter = 0
-        # Pause pet detection by setting "pause" flag
-        pause = 1
-
-    # If pet has been detected outside for more than 10 frames, set detected_outside flag
-    # and send a text to the phone.
-    if outside_counter > 10:
-        detected_outside = True
-        message = client.messages.create(
-            body = 'Your pet wants inside!',
-            from_=twilio_number,
-            to=my_number
-            )
-        inside_counter = 0
-        outside_counter = 0
-        # Pause pet detection by setting "pause" flag
-        pause = 1
-
-    # If pause flag is set, draw message on screen.
-    if pause == 1:
-        if detected_inside == True:
-            cv2.putText(frame,'Pet wants outside!',(int(IM_WIDTH*.1),int(IM_HEIGHT*.5)),font,3,(0,0,0),7,cv2.LINE_AA)
-            cv2.putText(frame,'Pet wants outside!',(int(IM_WIDTH*.1),int(IM_HEIGHT*.5)),font,3,(95,176,23),5,cv2.LINE_AA)
-
-        if detected_outside == True:
-            cv2.putText(frame,'Pet wants inside!',(int(IM_WIDTH*.1),int(IM_HEIGHT*.5)),font,3,(0,0,0),7,cv2.LINE_AA)
-            cv2.putText(frame,'Pet wants inside!',(int(IM_WIDTH*.1),int(IM_HEIGHT*.5)),font,3,(95,176,23),5,cv2.LINE_AA)
-
-        # Increment pause counter until it reaches 30 (for a framerate of 1.5 FPS, this is about 20 seconds),
-        # then unpause the application (set pause flag to 0).
-        pause_counter = pause_counter + 1
-        if pause_counter > 30:
-            pause = 0
-            pause_counter = 0
-            detected_inside = False
-            detected_outside = False
-
-    # Draw counter info
-    cv2.putText(frame,'Detection counter: ' + str(max(inside_counter,outside_counter)),(10,100),font,0.5,(255,255,0),1,cv2.LINE_AA)
-    cv2.putText(frame,'Pause counter: ' + str(pause_counter),(10,150),font,0.5,(255,255,0),1,cv2.LINE_AA)
-
-    return frame
-
-#### Initialize camera and perform object detection ####
-
+# Initialize camera and perform object detection.
 # The camera has to be set up and used differently depending on if it's a
 # Picamera or USB webcam.
+
+# I know this is ugly, but I basically copy+pasted the code for the object
+# detection loop twice, and made one work for Picamera and the other work
+# for USB.
 
 ### Picamera ###
 if camera_type == 'picamera':
@@ -241,26 +123,37 @@ if camera_type == 'picamera':
     rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
     rawCapture.truncate(0)
 
-    # Continuously capture frames and perform object detection on them
     for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 
         t1 = cv2.getTickCount()
         
         # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
         # i.e. a single-column array, where each item in the column has the pixel RGB value
-        frame = frame1.array
+        frame = np.copy(frame1.array)
         frame.setflags(write=1)
+        frame_expanded = np.expand_dims(frame, axis=0)
 
-        # Pass frame into pet detection function
-        frame = pet_detector(frame)
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: frame_expanded})
 
-        # Draw FPS
+        # Draw the results of the detection (aka 'visulaize the results')
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.40)
+
         cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
 
         # All the results have been drawn on the frame, so it's time to display it.
         cv2.imshow('Object detector', frame)
 
-        # FPS calculation
         t2 = cv2.getTickCount()
         time1 = (t2-t1)/freq
         frame_rate_calc = 1/time1
@@ -274,14 +167,12 @@ if camera_type == 'picamera':
     camera.close()
 
 ### USB webcam ###
-    
 elif camera_type == 'usb':
     # Initialize USB webcam feed
     camera = cv2.VideoCapture(0)
     ret = camera.set(3,IM_WIDTH)
     ret = camera.set(4,IM_HEIGHT)
 
-    # Continuously capture frames and perform object detection on them
     while(True):
 
         t1 = cv2.getTickCount()
@@ -289,17 +180,29 @@ elif camera_type == 'usb':
         # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
         # i.e. a single-column array, where each item in the column has the pixel RGB value
         ret, frame = camera.read()
+        frame_expanded = np.expand_dims(frame, axis=0)
 
-        # Pass frame into pet detection function
-        frame = bird_ID(frame)
+        # Perform the actual detection by running the model with the image as input
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: frame_expanded})
 
-        # Draw FPS
+        # Draw the results of the detection (aka 'visulaize the results')
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.85)
+
         cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
-
+        
         # All the results have been drawn on the frame, so it's time to display it.
         cv2.imshow('Object detector', frame)
 
-        # FPS calculation
         t2 = cv2.getTickCount()
         time1 = (t2-t1)/freq
         frame_rate_calc = 1/time1
@@ -309,5 +212,5 @@ elif camera_type == 'usb':
             break
 
     camera.release()
-        
+
 cv2.destroyAllWindows()
