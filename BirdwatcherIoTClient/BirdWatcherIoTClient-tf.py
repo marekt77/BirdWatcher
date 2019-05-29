281cd4db5beb56504c3fdb6a586a50ff51233cc0
Marek Tyrpa <mtyrpa@outlook.com>
2019-05-28 22:10:43 -0500

    changed image size

--- BirdwatcherIoTClient/BirdWatcherIoTClient-tf.py
+++ BirdwatcherIoTClient/BirdWatcherIoTClient-tf.py
@@ -26,8 +26,8 @@ import sys
 # Set up camera constants
 IM_WIDTH = 1280
 IM_HEIGHT = 720
-#IM_WIDTH = 640    Use smaller resolution for
-#IM_HEIGHT = 480   slightly faster framerate
+#IM_WIDTH = 640
+#IM_HEIGHT = 480
 
 # Select camera type (if user enters --usbcam when calling this script,
 # a USB webcam will be used)
