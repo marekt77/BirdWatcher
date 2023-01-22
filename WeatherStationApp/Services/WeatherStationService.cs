0760de781c70f6397ecb7565f47f3158cd564c71
Marek Tyrpa <mtyrpa@outlook.com>
2023-01-21 22:21:38 -0600

    Update

--- WeatherStationApp/Services/WeatherStationService.cs
+++ WeatherStationApp/Services/WeatherStationService.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherStationApp.Models;
 using WeatherStationApp.Services.Interface;
 
 namespace WeatherStationApp.Services
 {
     public interface WeatherStationService : IWeatherStationService
     {
+        private readonly HttpClient _client;
+        private readonly Uri _uri = new Uri("http://" + +  "/api/");
 
+        public async Task<SunTrack> GetSunTrackInfo()
+        {
+            try
+            {
+                var response = await _client.GetAsync(_uri);
+            }
+        }
     }
 }
