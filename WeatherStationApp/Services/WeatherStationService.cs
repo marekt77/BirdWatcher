f2736b3d48b1a4ab5bb245467eafd5081e2fc1da
Marek Tyrpa <mtyrpa@outlook.com>
2023-01-22 22:03:33 -0600

    small update

--- WeatherStationApp/Services/WeatherStationService.cs
+++ WeatherStationApp/Services/WeatherStationService.cs
@@ -10,15 +10,28 @@ namespace WeatherStationApp.Services
 {
     public interface WeatherStationService : IWeatherStationService
     {
-        private readonly HttpClient _client;
-        private readonly Uri _uri = new Uri("http://" + +  "/api/");
+        private Uri _uri = new Uri("http://" + "192.168.1.20" + "/api/suntrack");
 
         public async Task<SunTrack> GetSunTrackInfo()
         {
+            HttpClient _client = new HttpClient();
+            SunTrack sunTrack = new SunTrack();
+
             try
             {
                 var response = await _client.GetAsync(_uri);
+
+                HttpResponseMessage httpResponse = new HttpResponseMessage();
+
+
             }
+            catch (Exception ex)
+            {
+                _client.Dispose();
+                throw new Exception("ERROR! Cannot retrive Movie Data");
+            }
+
+            return sunTrack;
         }
     }
 }
