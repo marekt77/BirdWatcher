8dc25f73df68f9ec59f0621b61c1cba96f9595c5
Marek Tyrpa <mtyrpa@outlook.com>
2024-02-09 12:53:43 -0600

    spelling fix

--- BirdWatcherWeb/Helpers/PaginationHelper.cs
+++ BirdWatcherWeb/Helpers/PaginationHelper.cs
@@ -15,22 +15,26 @@ namespace BirdWatcherWeb.Helpers
             IUriService uriService, 
             string route)
         {
-            var respose = new PagedResponse<List<T>>(pagedData, validFilter.PageNumber, validFilter.PageSize);
+            var response = new PagedResponse<List<T>>(pagedData, validFilter.PageNumber, validFilter.PageSize);
             var totalPages = ((double)totalRecords / (double)validFilter.PageSize);
             int roundedTotalPages = Convert.ToInt32(Math.Ceiling(totalPages));
-            respose.NextPage =
+
+            response.NextPage =
                 validFilter.PageNumber >= 1 && validFilter.PageNumber < roundedTotalPages
                 ? uriService.GetPageUri(new PaginationFilter(validFilter.PageNumber + 1, validFilter.PageSize), route)
                 : null;
-            respose.PreviousPage =
+
+            resresponsepose.PreviousPage =
                 validFilter.PageNumber - 1 >= 1 && validFilter.PageNumber <= roundedTotalPages
                 ? uriService.GetPageUri(new PaginationFilter(validFilter.PageNumber - 1, validFilter.PageSize), route)
                 : null;
-            respose.FirstPage = uriService.GetPageUri(new PaginationFilter(1, validFilter.PageSize), route);
-            respose.LastPage = uriService.GetPageUri(new PaginationFilter(roundedTotalPages, validFilter.PageSize), route);
-            respose.TotalPages = roundedTotalPages;
-            respose.TotalRecords = totalRecords;
-            return respose;
+
+            response.FirstPage = uriService.GetPageUri(new PaginationFilter(1, validFilter.PageSize), route);
+            response.LastPage = uriService.GetPageUri(new PaginationFilter(roundedTotalPages, validFilter.PageSize), route);
+            response.TotalPages = roundedTotalPages;
+            response.TotalRecords = totalRecords;
+
+            return response;
         }
     }
 }
